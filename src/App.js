f43d72f632b3e4db10ee7f5665c7cb7dcd8d8b75
codeblooded47 <mohdhassandbg@gmail.com>
2022-10-28 01:50:19 +0530

    update css

--- src/App.js
+++ src/App.js
@@ -1,45 +1,40 @@
- import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "./comp/Navbar/navbar"
+import NavBar from "./comp/Navbar/navbar";
 //import TeamMember from "./comp/TeamMember/TeamMember"
 import { Helmet } from "react-helmet";
 
-import Home from "./pages/home"
-import Mint from "./pages/mint"
+import Home from "./pages/home";
+import Mint from "./pages/mint";
 import NewsLetter from "./comp/newsLetter/newsLetter";
-import Footer from "./comp/footer/footer"
+import Footer from "./comp/footer/footer";
 import Roadmap from "./pages/roadmap";
 
+const App = () => {
+  return (
+    <Router>
+      <Helmet>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Khula:wght@400;600;800&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/mint" element={<Mint />} />
+        <Route path="/roadmap" element={<Roadmap />} />
+      </Routes>
 
-const App= () => {
-    return (
-        
-            
-        <Router>
-            <Helmet>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-                <link href="https://fonts.googleapis.com/css2?family=Khula:wght@400;600;800&display=swap" rel="stylesheet" />
-            </Helmet>
-            <NavBar/>
-            <Routes>
-
-                <Route path="/" element={<Home />} />
-                <Route path="/mint" element={<Mint />} />
-                <Route path="/roadmap" element={<Roadmap />} />
-                    
-            </Routes>
-
-            <div className={"Container"}>
-                <NewsLetter></NewsLetter>
-                <Footer></Footer>
-
-            </div>
-        </Router>
-        
-       
-    )
-}
+      <div className={"Container"}>
+        <NewsLetter></NewsLetter>
+        <Footer></Footer>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
